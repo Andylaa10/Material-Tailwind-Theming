7242e46390b91d2046f4c3e5cd49e3af4eee551f
Andy lam <91528350+Andylaa10@users.noreply.github.com>
2024-10-22 18:02:22 +0200

    updated dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import {
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
   ],
 };
